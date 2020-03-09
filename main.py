de4f6dbead57846ba437bbe54f611273b02289b5
NMan1 <nickmantini01@gmail.com>
2020-03-08 21:52:17 -0500

    Added webhook

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 from utilites.TxtFileHelper import AddLineBreaks
 from utilites.GithubScraper import get_github_projects
 from utilites.AccountCracker import run
+from git import Repo 
+from flask.json import jsonify
+import hmac
+import hashlib
 import threading
 import time
-from flask.json import jsonify
+
 
 app = Flask(__name__)
+
+
+def is_valid_signature(x_hub_signature, data, private_key):
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 
 @app.route("/home")
 def home():
     project_list = get_github_projects()
     return render_template("home.html", value=project_list)
+
+
+@app.route('/update_server', methods=['POST'])
+def webhook():
+    if request.method == 'POST':
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        if not is_valid_signature(x_hub_signature, request.data, "Mantini88"):
+            print('Deploy signature failed: {sig}'.format(sig=x_hub_signature))
+            abort(418)
+
+        repo = Repo('https://github.com/NMan1/OverflowWebsite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    else:
+        return 'Wrong event type', 400
 
 
 @app.route("/github")
